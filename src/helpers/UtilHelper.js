5444bd7761afdface2d51d13320cf8b83d349609
sandeep <iamabornprogrammer@gmail.com>
2022-03-17 15:18:02 +0530

    added some helper function

--- src/helpers/UtilHelper.js
+++ src/helpers/UtilHelper.js
@@ -6,4 +6,12 @@ export default class UtilHelper {
   generateRandomString(strLength) {
     return Buffer.from(Math.random().toString()).substr(10, strLength);
   }
+
+  isArrayValid(arr) {
+    return arr && arr instanceof Array && arr.length > 0;
+  }
+
+  isObjecyValid(obj) {
+    return obj && obj instanceof Object && Object.keys(obj).length > 0;
+  }
 }
