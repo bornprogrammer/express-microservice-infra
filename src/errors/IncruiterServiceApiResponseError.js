774e8f58c2f517d5ec8879fa3cee98f24023bea5
sandeep <iamabornprogrammer@gmail.com>
2022-04-07 10:42:17 +0530

    made changes

--- src/errors/IncruiterServiceApiResponseError.js
+++ src/errors/IncruiterServiceApiResponseError.js
@@ -3,6 +3,6 @@ import CustomAxiosError from "./CustomAxiosError.js";
 export default class IncruiterServiceApiResponseError extends CustomAxiosError {
 
   constructor(customAxiosError) {
-    super({ message: customAxiosError.message, response: { status: customAxiosError.code, data: customAxiosError.data.result } });
+    super({ message: customAxiosError.data.message, response: { status: customAxiosError.code, data: customAxiosError.data.result } });
   }
 }
