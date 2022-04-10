5126291ee0c18c20e26dec901f0ef31251fdce3c
sandeep <iamabornprogrammer@gmail.com>
2022-04-10 12:12:00 +0530

    committed

--- src/helpers/MongooseWhereCluaseHelper.js
+++ src/helpers/MongooseWhereCluaseHelper.js
@@ -5,7 +5,7 @@ class MongooseWhereCluaseHelper {
 
   // will active and inactive any entity if they are not deleted
   updateEntityStatusById(entityId, status) {
-    return this.findByIdAndUpdate(entityId, { status: { $ne: EntityStatus.DELETED } }, { status }, { new: true });
+    return this.findOneAndUpdate({ _id: entityId, status: { $ne: EntityStatus.DELETED } }, { status }, { new: true });
   }
 
   //
