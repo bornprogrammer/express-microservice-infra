cb0ed1531e9b6da9a87e428520a87bc4d2f06711
sandeep <iamabornprogrammer@gmail.com>
2022-04-26 08:37:08 +0530

    added mongoose where clause helper method,app service helper metho

--- src/app-services/BaseAppService.js
+++ src/app-services/BaseAppService.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable dot-notation */
 
+import mongoose from "mongoose";
 import HttpResponseStatus from "../constants/HttpResponseStatus.js";
 import IncruiterServiceApiResponseError from "../errors/IncruiterServiceApiResponseError.js";
 import HttpHelper from "../helpers/HttpHelper.js";
 
+const { ObjectId } = mongoose.Types;
 // will be extended by child app classed to be used for calling another micro service
 export default class BaseAppService {
 
@@ -90,5 +94,77 @@ export default class BaseAppService {
   async patch() {
     const result = await this.call("patch");
     return result;
+  }
+
+
+  /**
+   * will take the mongoose collections
+   * @param {} collection 
+   * @param {*} key 
+   * @returns 
+   */
+  async mergeResouresToCollections(collection, resources, collectionKey, resourceKey = "_id") {
+    if (collection && resources && collection instanceof Array && resources instanceof Array && collection.length > 0 && resources.length > 0) {
+      return collection.map((item) => {
+        const collectionItemValue = item[collectionKey];
+        const findById = (resourcesList, id) => resourcesList.find((resourceItem) => resourceItem["_id"] === id.toString());
+        let mappedResources;
+        if (collectionItemValue && collectionItemValue instanceof Array && collectionItemValue.length > 0) {
+          mappedResources = collectionItemValue.map((i) => {
+            const findByIdResult = findById(resources, i);
+            return findByIdResult || i;
+          })
+        } else if (collectionItemValue && ObjectId.isValid(collectionItemValue)) {
+          mappedResources = findById(resources, collectionItemValue);
+          mappedResources = mappedResources || collectionItemValue;
+        } else {
+          mappedResources = collectionItemValue;
+        }
+        item["_doc"][collectionKey] = mappedResources;
+        return item;
+      });
+    }
+    return collection;
+  }
+
+  async fetchNMergeResouresToCollections(collection, collectionKey, resourceKey = "_id") {
+    const fetchedResources = await this.fetchResouresByCollections(collection, collectionKey);
+    const mergedResult = await this.mergeResouresToCollections(collection, fetchedResources, collectionKey, resourceKey);
+    return mergedResult;
+  }
+
+  async fetchResouresByCollections(collection, collectionKey) {
+    const collectionIds = await this.extractOutIdsFromCollection(collection, collectionKey);
+    const result = await this.getResourcesByMultipleIds(collectionIds);
+    return result;
+  }
+
+  /**
+   * will extract out all ids from collection
+   * @param {*} collection 
+   * @param {*} collectionKey 
+   * @returns 
+   */
+  async extractOutIdsFromCollection(collection, collectionKey) {
+    if (collection && collection instanceof Array && collection.length > 0) {
+      let idsArray = [];
+      collection.forEach((item) => {
+        if (item[collectionKey] && item[collectionKey] instanceof Array && item[collectionKey].length > 0) {
+          idsArray = [...item[collectionKey]];
+        }
+        idsArray = [...idsArray, item[collectionKey]];
+      });
+      return idsArray;
+    }
+    return [];
+  }
+
+  async getResourcesByMultipleIds(idsArray) {
+    if (idsArray && idsArray instanceof Array && idsArray.length > 0) {
+      const commaSeperatedIds = idsArray.join(",");
+      const result = await this.setPath([commaSeperatedIds]).get();
+      return result;
+    }
+    return [];
   }
 }
--- src/helpers/MongooseWhereCluaseHelper.js
+++ src/helpers/MongooseWhereCluaseHelper.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 
 import EntityStatus from "../constants/EntityStatus.js";
 
@@ -8,13 +9,47 @@ class MongooseWhereCluaseHelper {
     return this.findOneAndUpdate({ _id: entityId, status: { $ne: EntityStatus.DELETED } }, { status }, { new: true });
   }
 
+  updateEntityStatus(query, status) {
+    const findOneAndUpdateQuery = { status: { $ne: EntityStatus.DELETED }, ...query };
+    return this.findOneAndUpdate(findOneAndUpdateQuery, { status }, { new: true });
+  }
+
   updateEntityById(entityId, payload) {
     return this.findOneAndUpdate({ _id: entityId, status: EntityStatus.ACTIVE }, payload, { new: true });
   }
 
-  // will use it to return the list with paginatin
-  getList() {
+  updateEntity(query, payload) {
+    const findOneAndUpdateQuery = { status: EntityStatus.ACTIVE, ...query };
+    return this.findOneAndUpdate(findOneAndUpdateQuery, payload, { new: true });
+  }
 
+  deleteEntityById(entityId, payload) {
+    return this.findOneAndUpdate({ _id: entityId, status: EntityStatus.ACTIVE }, payload, { new: true });
+  }
+
+  deleteEntity(query, payload) {
+    const findOneAndUpdateQuery = { status: EntityStatus.ACTIVE, ...query };
+    return this.findOneAndUpdate(findOneAndUpdateQuery, payload, { new: true });
+  }
+
+  // will use it to return the list with pagination and search
+  async list(query) {
+    const findQuery = { status: EntityStatus.ACTIVE, ...query };
+    const count = await this.countDocuments(findQuery);
+    let result = {};
+    if (count > 0) {
+      const page = query?.page ? query?.page : 1;
+      const pageSize = query?.pageSize || 10;
+      const skip = (page - 1) * query?.pageSize;
+      const paginationParams = { sort: { _id: "desc" }, limit: pageSize, skip };
+      delete query?.page;
+      delete query?.pageSize;
+      result = await this.find(findQuery).sort(paginationParams.sort).limit(paginationParams.limit).skip(paginationParams.skip);
+      result = {
+        data: result, total: count, page, pageSize,
+      };
+    }
+    return result;
   }
 
   getAdminList() {
