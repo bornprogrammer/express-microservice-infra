a91a9f566ba191ba66c1c23d409a62433e41d0c4
sandeep <iamabornprogrammer@gmail.com>
2022-04-27 19:44:58 +0530

    made changes to s3 file uploader

--- src/helpers/S3BucketFileUploader.js
+++ src/helpers/S3BucketFileUploader.js
@@ -4,14 +4,14 @@ import fsPromise from "fs/promises";
 class S3BucketFileUploader {
 
   s3Bucket = new AWS.S3({
-    accessKeyId: process.env.aws_access_key,
-    secretAccessKey: process.env.aws_secret_access_key,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
 
   async upload(multerFileObject, bucketName, bucketFolderName) {
-    const fileContent = await fsPromise.readFile(multerFileObject.path, { encoding: "utf-8" });
+    const fileContent = await fsPromise.readFile(multerFileObject.path);
     const bucketNameWithFolderName = `${bucketName}/${bucketFolderName}`;
-    const params = { Bucket: bucketNameWithFolderName, Key: multerFileObject.filename, Body: JSON.stringify(fileContent, null, 2), CreateBucketConfiguration: { LocationConstraint: process.env.aws_region } };
+    const params = { Bucket: bucketNameWithFolderName, Key: multerFileObject.filename, Body: fileContent, CreateBucketConfiguration: { LocationConstraint: process.env.aws_region } };
     const result = await this.s3Bucket.upload(params).promise();
     return result;
   }
